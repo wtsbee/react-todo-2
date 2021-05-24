376d334d1d41a4ea753496b1d936e5fb62ed3d27
wtsbee <47000392+wtsbee@users.noreply.github.com>
2021-05-24 23:21:09 +0900

    コンポーネントファイルの削除

--- src/components/InputTodo.jsx
+++
@@ -1,9 +0,0 @@
-// export const InputTodo = () => {
-//   const onClickAdd =
-//   return (
-//     <div className="input-area">
-//       <input placeholder="TODOを入力"></input>
-//       <button onClick={onClickAdd}>追加</button>
-//     </div>
-//   );
-// };
