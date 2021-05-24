8031bd072cb9c5a8d03293287c65123696051c9c
wtsbee <47000392+wtsbee@users.noreply.github.com>
2021-05-24 23:05:17 +0900

    Initial commit

---
+++ src/components/InputTodo.jsx
@@ -0,0 +1,9 @@
+// export const InputTodo = () => {
+//   const onClickAdd =
+//   return (
+//     <div className="input-area">
+//       <input placeholder="TODOを入力"></input>
+//       <button onClick={onClickAdd}>追加</button>
+//     </div>
+//   );
+// };
